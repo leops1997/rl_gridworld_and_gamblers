a46416ee7c4e84fd4d34fde830bfbb5c1b46b242
leops1997 <leops1997@gmail.com>
2023-06-18 19:10:37 -0300

    Adding gridworld file

---
+++ gridworld.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Entrega #4 - Dynamic Programming - Policy Evaluation Gridworld
+
+@author: Leonardo Pezenatto da Silva
+@email: leonardo.pezenatto@posgrad.ufsc.br
+@date: Jun 20, 2023
+"""
+
+import numpy as np
+import matplotlib as plt
+
+def policy_evaluation():
+    pass
+
+def plot_matrix():
+    pass
+
+if __name__ == "__main__":
+ pass
