b78a3c1e9e8574a680cd74e7a6ccda80b51779da
leops1997 <leops1997@gmail.com>
2023-06-18 19:54:55 -0300

    gamblers working and commented

--- gamblers_problem.py
+++ gamblers_problem.py
@@ -5,51 +5,80 @@ Entrega #4 - Dynamic Programming - Value iteration Gambler's Problem
 
 @author: Leonardo Pezenatto da Silva
 @email: leonardo.pezenatto@posgrad.ufsc.br
-@date: Jun 20, 2023
+@date: Jun 18, 2023
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+print(__doc__)
+
 class Gambler:
+    """
+    Class Gambler.
+    Have all methods needed to implement the Gambler's problem
+    with value iteration.
+    """
+    
     def __init__(self, prob, iterations, theta=0.00000001):
-        self.value = np.zeros(101)
-        self.reward = np.zeros(101)
-        self.reward[100] = 1
+        self.value = np.zeros(101) # Set a list of 100 values 0
+        self.reward = np.zeros(101) # Set a list of 100 values 0
+        self.reward[100] = 1 # Set 1 on the last element
         self.values_recorded = []
         self.pi = []
         self.prob = prob
         self.iterations = iterations
         self.theta = theta
-        
 
     def value_iteration(self):
+        """
+        Value iteration, for estimating policy.
+        Theta will determine accuracy of estimation
+        """
         delta = 0
         p = np.zeros(101)
         while delta < self.theta:
             for capital in range(1,100):
+                # Store the previous value of current state
                 previous_value = self.value[capital]
-                for bet in range(1, min(capital, 100 - capital)+1): # Bet value with minimum of 1 and maximum of 100 - capital
+                # Bet value with minimum of 1 and maximum of 100 - capital
+                for bet in range(1, min(capital, 100 - capital)+1): 
+                    # Calculates the value of a bet(action) from a current amount of money(state)
                     p[bet] = self.prob*(self.reward[capital + bet] + self.value[capital + bet]) + (1-self.prob)*(self.reward[capital - bet] + self.value[capital - bet])
+                # Update the maximum value
                 self.value[capital] = max(p)
+                # Update delta
                 delta = max(delta, abs(previous_value - self.value[capital]))
+        # Store the new values
         self.values_recorded.append(self.value.copy())
                 
     def policy(self):
+        """
+        Output the optimal policy
+        """
         self.pi = []
         p = np.zeros(101)
         for capital in range(1,100):
-            for bet in range(1, min(capital, 100 - capital)+1): # Bet value with minimum of 1 and maximum of 100 - capital
+            # Bet value with minimum of 1 and maximum of 100 - capital
+            for bet in range(1, min(capital, 100 - capital)+1): 
+                # Calculates the value of a bet(action) from a current amount of money(state)
                 p[bet] = self.prob*(self.reward[capital + bet] + self.value[capital + bet]) + (1-self.prob)*(self.reward[capital - bet] + self.value[capital - bet])
+            # Store the new optimal policy
             self.pi.append(np.argmax(p))
 
     def compute(self):
+        """
+        Compute the successive iterations and the final policy
+        """
         for i in range(self.iterations):
             self.value_iteration()
             self.policy()
         self.plot_results()
 
     def plot_results(self):
+        """
+        Plot on a graph the value function by successive iterations and the final policy
+        """
         plt.subplot(2, 1, 1)
         for data in self.values_recorded:
             plt.plot(data[:99])
